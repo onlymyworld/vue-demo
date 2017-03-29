b3baa857edcb21abf8c89458d2b7cbd01cc389b4
onlymyworld <1208954839@qq.com>
2017-03-29 18:13:02 +0800

    commit all

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+	* Created by YueFang on 2017/3/29.
+	*/
+
+import Vue from "vue"
+import store from './vuex/store'
+import App from './components/App.vue'
+
+new Vue({
+	store,
+	el:'body',
+	components:{ App }
+})
